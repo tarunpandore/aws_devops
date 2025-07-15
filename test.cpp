4307590d8d2f82536db4af1b8079fde6f366b03a
Rishab <rishab@gmail.com>
2025-07-15 14:39:35 +0530

    This is my Second Commit

--- test.cpp
+++ test.cpp
@@ -5,5 +5,7 @@ int main() {
     for (int i = 1; i < 11; i++) {
         cout << "Number: " << i << endl;
     }
+    cout << "Loop completed!" << endl;
+    cout << "Program has Ended.";
     return 0;
 }
